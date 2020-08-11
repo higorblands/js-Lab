e363bb1560cfbff368e8ce4a97d2dfa5180d640b
higorblands <67286451+higorblands@users.noreply.github.com>
2020-08-11 09:47:03 -0300

    Update loveCalculate.js

--- loveCalculate.js
+++ loveCalculate.js
@@ -1,7 +1,20 @@
+// Names input
 var stName = prompt('Whats your name ?');
 var ndName = prompt('Whats the name of you crush ?');
 
+// logic to generate a random number beetween 1 and 100.
 var loveScore = Math.random();
 loveScore = loveScore * 100;
 loveScore = Math.floor(loveScore) + 1;
-console.log(stName +' you matched ' +loveScore+ '% with ' + ndName + '.' );
+
+// output
+
+if(loveScore > 70){
+    console.log(stName +' you matched ' +loveScore+ '% with ' + ndName + ' this is a hot love.' );
+}
+if(loveScore < 70 && loveScore > 50){
+    console.log(stName + 'you matched ' +loveScore+ '% with ' + ndName + ' this is normal love.');
+}
+if(loveScore <= 49){
+    console.log(stName + 'you matched ' +loveScore+ '% with ' + ndName + ' this love needs more fire.');
+}
